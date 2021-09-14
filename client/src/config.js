981887fadd70e6457e5501b2a60bad6b29d8e406
Austin <Harrenaustint@gmail.com>
2021-09-14 16:25:08 -0400

    push

--- client/src/config.js
+++ client/src/config.js
@@ -1,5 +1,5 @@
 import dotenv from 'dotenv'
 dotenv.config()
-const BACKEND_URL = process.env.NODE_ENV === 'development'? "http://localhost:5000":"https://deploy-4-u.herokuapp.com/"
+const BACKEND_URL = process.env.NODE_ENV === 'development'? "http://localhost:5000":"https://deploy-4-u.herokuapp.com"
 
 export default BACKEND_URL;
