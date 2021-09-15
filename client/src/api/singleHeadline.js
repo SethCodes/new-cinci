827a95ed8f23841668c833d0ac74ada01ffb544d
Seth Stephens <sethastephens8@gmail.com>
2021-09-15 14:06:27 -0400

    img fix news pull

--- client/src/api/singleHeadline.js
+++ client/src/api/singleHeadline.js
@@ -1,16 +1,15 @@
 import React from "react";
-import '../components/About.css';
- 
-const SingleHeadline = ({item}) => (
-    <a id="column" className="col-md-3 col-sm-6" href={item.url}>
-        <div className="card">
-            <img className="img-fluid" src={item.urlToImage} alt="url-news"/>
-            <div className="card-body">
-                <p className="card-title">{item.title}</p>
-            </div>
-        </div>
-    </a>
- 
+import "../components/About.css";
+
+const SingleHeadline = ({ item }) => (
+  <a id="column" className="col-md-3 col-sm-6" href={item.url}>
+    <div className="card">
+      <img className="img-fluid" src={item.image} alt="url-news" />
+      <div className="card-body">
+        <p className="card-title">{item.title}</p>
+      </div>
+    </div>
+  </a>
 );
- 
+
 export default SingleHeadline;
